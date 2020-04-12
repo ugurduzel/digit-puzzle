411b51b9d7c6285c00e92e83f1f26a62866ff705
Ugur <ugurduzel@gmail.com>
2020-04-12 23:20:23 +0300

    a

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.post("/new-message", function (req, res) {
 
     const { message } = req.body;
 
+    return res.json(req);
+
     //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
     // if (!message || message.text.toLowerCase().indexOf("marco") < 0) {
@@ -28,7 +30,7 @@ app.post("/new-message", function (req, res) {
     // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
     axios
         .post(
-            "https://api.telegram.org/973273396:AAEbU_Nj4z3pwCM-nnyk0inizNWH2ys5hk4/sendMessage",
+            "https://api.telegram.org/bot973273396:AAEbU_Nj4z3pwCM-nnyk0inizNWH2ys5hk4/sendMessage",
             {
                 chat_id: message.chat.id,
                 text: "Helloo!!",
@@ -50,3 +52,5 @@ app.post("/new-message", function (req, res) {
 app.listen(3000, function () {
     console.log("Telegram app listening on port 3000!");
 });
+
+// curl -F "url=https://pacific-journey-79915.herokuapp.com/new-message" "https://api.telegram.org/bot973273396:AAEbU_Nj4z3pwCM-nnyk0inizNWH2ys5hk4/setWebhook"
