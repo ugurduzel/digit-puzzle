095c3f90186c8b9dd2abc44e023e751bee4da9d0
Ugur <ugurduzel@gmail.com>
2020-04-13 00:13:06 +0300

    b

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply("Welcome to Digit Puzzle!"));
 
-bot.command("newgame", ({ ctx }) => {
+bot.command("newgame", (ctx) => {
     console.log(ctx);
     ctx.reply("Choose a difficulty level\n3 digits\n4 digits\n5 digits");
 });
