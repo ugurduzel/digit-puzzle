f3c17fa30a965e4b39662e71eba8d01444f0ef43
Ugur <ugurduzel@gmail.com>
2020-04-12 23:41:03 +0300

    a

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-var telegram = require("telegram-bot-api");
+const telegram = require("telegram-bot-api");
 
-var api = new telegram({
+const api = new telegram({
     token: "973273396:AAEbU_Nj4z3pwCM-nnyk0inizNWH2ys5hk4",
     updates: {
         enabled: true,
     },
 });
 
+async function sendMessage(chat_id) {
+    try {
+        const result = await api.sendMessage({
+            chat_id: chat_id,
+            text: "This is my kind message to you",
+        });
+        console.log(result);
+    } catch (ex) {
+        console.log("Error: ex");
+    }
+}
+
 api.on("message", function (message) {
     // Received text message
-    console.log(message);
+    console.log("Hey, message received\n", message);
 
-    api.sendMessage({
-        chat_id: message.chat_id,
-        text: message,
-    })
-        .then(function (data) {
-            console.log(util.inspect(data, false, null));
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+    await sendMessage(message.chat.id);
 });
