63e537c8c8c6633340fc20f7dc5b2eee2a7c8465
Ugur <ugurduzel@gmail.com>
2020-04-13 18:03:16 +0300

    App

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const ongoingScene = require("./scenes/ongoingScene");
 
 // Express REST app
 
-const URL = process.env.BOT_URL || "http://142.93.175.101";
+const URL = "https://142.93.175.101";
 const port = process.env.PORT || 8080;
 const API_TOKEN = process.env.BOT_TOKEN || "";
 
