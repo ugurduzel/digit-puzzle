e023be7cd051e0725f635983e5852d667dbed81b
Ugur <ugurduzel@gmail.com>
2020-04-14 22:15:00 +0300

    Working

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const stage = new Stage([navigationScene, sp_beginScene, sp_ongoingScene]);
 const bot = new Telegraf(process.env.BOT_TOKEN || "");
 
 bot.use(commandParts());
-//bot.use(howto());
-//bot.use(gameModel.middleware());
+bot.use(howto());
+bot.use(gameModel.middleware());
 bot.use(sessionModel.middleware());
 bot.use(underMaintenanceMiddleware());
 bot.use(stage.middleware());
