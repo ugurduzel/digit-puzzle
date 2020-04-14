89b880444a9e5ff1deaefc15a4e2ceff39c2b88e
Ugur <ugurduzel@gmail.com>
2020-04-14 16:42:14 +0300

    dc

--- middleware/howto.js
+++ middleware/howto.js
@@ -21,7 +21,7 @@ const message =
     You need to make intelligent guesses. That is the fun part. 😁";
 const howto = () => (ctx, next) => {
     console.log(ctx);
-    if (ctx.state.command.command === "howto") {
+    if (ctx.state.command && ctx.state.command.command === "howto") {
         return ctx.reply(message);
     }
 };
