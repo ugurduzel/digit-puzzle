7e61fa47c25cb8871b9a1b766d17aeeb1093cb91
Ugur <ugurduzel@gmail.com>
2020-04-14 20:38:55 +0300

    dc

--- index.js
+++ index.js
@@ -25,23 +25,23 @@ const bot = new Telegraf(process.env.BOT_TOKEN || "");
 
 bot.use(commandParts());
 //bot.use(howto());
-bot.use(gameModel.middleware());
+//bot.use(gameModel.middleware());
 bot.use(sessionModel.middleware());
 bot.use(underMaintenanceMiddleware());
 bot.use(stage.middleware());
 //bot.use(log());
 
 bot.action("NEW_GAME", (ctx) => {
-    if (!ctx.game.players) {
-        ctx.game.players = {};
-    }
-    if (ctx.game.players[ctx.from.id]) {
-        console.log("Player " + ctx.from.id + " is found\n" + ctx.game.players[ctx.from.id]);
-    } else {
-        ctx.game.players[ctx.from.id] = {};
-        console.log("Adding a player with id" + ctx.from.id);
-        ctx.reply("We have added you to our userbase. 👏\n\nHave fun! ");
-    }
+    // if (!ctx.game.players) {
+    //     ctx.game.players = {};
+    // }
+    // if (ctx.game.players[ctx.from.id]) {
+    //     console.log("Player " + ctx.from.id + " is found\n" + ctx.game.players[ctx.from.id]);
+    // } else {
+    //     ctx.game.players[ctx.from.id] = {};
+    //     console.log("Adding a player with id" + ctx.from.id);
+    //     ctx.reply("We have added you to our userbase. 👏\n\nHave fun! ");
+    // }
 
     return ctx.scene.enter("navigationScene");
 });
