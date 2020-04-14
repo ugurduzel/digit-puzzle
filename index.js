ef216d6ed22a16dd2b28845e9b617a70d6e740f0
Ugur <ugurduzel@gmail.com>
2020-04-14 14:19:28 +0300

    Added maintain middleware

--- index.js
+++ index.js
@@ -1,65 +1,43 @@
 const Telegraf = require("telegraf");
-const Extra = require("telegraf/extra");
-const Markup = require("telegraf/markup");
-const session = require("telegraf/session");
 const Stage = require("telegraf/stage");
-const Scene = require("telegraf/scenes/base");
-const _ = require("lodash");
+const Extra = require("telegraf/extra");
+const session = require("telegraf/session");
 
+// Middlewares
+const underMaintenance = require("./middleware/maintenance");
+const commandArgsMiddleware = require("./middleware/commandArgs");
+
+// Scenes
 const beginScene = require("./scenes/singleplayer/beginScene");
 const ongoingScene = require("./scenes/singleplayer/ongoingScene");
 
-const commandArgsMiddleware = require("./middleware/commandArgs");
-const { getResult, notDistinct, logMessage, getTime } = require("./utils");
-const { admin_from_id, underMaintenance, minLevel, maxLevel } = require("./configs/constants.json");
+const stage = new Stage([beginScene, ongoingScene]);
 
 const bot = new Telegraf(process.env.BOT_TOKEN || "");
 
-bot.use(session());
+bot.use(underMaintenance());
 bot.use(commandArgsMiddleware());
+bot.use(session());
 
-const stage = new Stage([beginScene, ongoingScene]);
 bot.use(stage.middleware());
 
-bot.command("newgame", (ctx) => {
-    console.log(ctx.from);
-    console.log(ctx.chat);
-    if (underMaintenance === true && ctx.from.id !== admin_from_id) {
-        logMessage(ctx.chat.first_name + " is trying to play.");
-        return ctx.reply("Game is under maintenance now");
-    }
-    return ctx.scene.enter("beginScene");
-});
+bot.command("newgame", (ctx) => ctx.scene.enter("beginScene"));
 
-bot.action("New Game", (ctx) => {
-    if (underMaintenance === true && ctx.from.id !== admin_from_id) {
-        logMessage(ctx.chat.first_name + " is trying to play.");
-        return ctx.reply("Game is under maintenance now");
-    }
-    return ctx.scene.enter("beginScene");
-});
+bot.action("New Game", (ctx) => ctx.scene.enter("beginScene"));
 
-bot.command("start", (ctx) => {
-    if (underMaintenance === true && ctx.from.id !== admin_from_id) {
-        logMessage(ctx.chat.first_name + " is trying to play.");
-        return ctx.reply("Game is under maintenance now");
-    }
-    return ctx.reply(
+bot.command("start", (ctx) =>
+    ctx.reply(
         `Hi ${ctx.chat.first_name},\nWelcome to Digit Puzzle!\n`,
         Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton("🎮 Play now!", "New Game")]))
-    );
-});
+    )
+);
 
-bot.on("message", (ctx) => {
-    if (underMaintenance === true && ctx.from.id !== admin_from_id) {
-        logMessage(ctx.chat.first_name + " is trying to play.");
-        return ctx.reply("Game is under maintenance now");
-    }
-    return ctx.reply(
+bot.on("message", (ctx) =>
+    ctx.reply(
         "Try /newgame",
         Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton("🎮 Play now!", "New Game")]))
-    );
-});
+    )
+);
 
 console.log("Launching the application... " + new Date(Date.now()).toTimeString().substring(0, 8));
 bot.launch();
---
+++ middleware/maintenance.js
@@ -0,0 +1,12 @@
+const { isUnderMaintenance, admin_from_id } = require("../configs/constants.json");
+const { logMessage } = require("../utils");
+
+underMaintenance = (ctx, next) => {
+    if (isUnderMaintenance === true && ctx.from.id !== admin_from_id) {
+        logMessage(ctx.chat.first_name + " is trying to play.");
+        return ctx.reply("Thank you for your message. 🤗\n\nGame is under maintenance now. But not for long... ⏳");
+    }
+    return next();
+};
+
+module.export = underMaintenance;
