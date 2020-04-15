9851dca495a384fda6c58c7a7814a1a72b3caace
Ugur <ugurduzel@gmail.com>
2020-04-15 19:34:10 +0300

    mp

--- middleware/mpFilter.js
+++ middleware/mpFilter.js
@@ -4,19 +4,16 @@ const Markup = require("telegraf/markup");
 
 const { storage: mpGame } = require("../cache");
 
-const
 const mpFilter = () => (ctx, next) => {
     if (ctx.chat.type === "group") {
-
-        if(mpGame.has(ctx.chat.id)) {
+        if (mpGame.has(ctx.chat.id)) {
             mpGame = mpGame.get(ctx.chat.id);
-            if(ctx.message.id === mpGame.user1.id || ctx.message.id === mpGame.user2.id) {
-                if(mpGame.get("turn") && ctx.message.id === mpGame.get("turn")) {
+            if (ctx.message.id === mpGame.user1.id || ctx.message.id === mpGame.user2.id) {
+                if (mpGame.get("turn") && ctx.message.id === mpGame.get("turn")) {
                     return next();
                 }
                 return ctx.reply("It's not your turn.", Extra.HTML().inReplyTo(ctx.message.message_id));
-            }
-            else {
+            } else {
                 return;
             }
         }
