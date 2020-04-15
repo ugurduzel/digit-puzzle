a3054b39bb85c3c9d8dc29bf5bfb165b93f93c22
Ugur <ugurduzel@gmail.com>
2020-04-15 03:06:57 +0300

    Gece Calisiyor - no under maintain

--- scenes/singleplayer/sp_ongoingScene.js
+++ scenes/singleplayer/sp_ongoingScene.js
@@ -95,7 +95,9 @@ sp_ongoingScene.hears(/.*/, (ctx) => {
         ctx.reply(
             `<b>Congrats!</b> 🎊🎉\n\nNumber is <b>${number.join(
                 ""
-            )}</b>.\nYou found it in ${guesses} tries. 🤯\n\n${getStepLeaderboard(ctx.gameStat.get("sp_step_top10"))}`,
+            )}</b>.\nYou found it in ${guesses} tries. 🤯\n\n${getStepLeaderboard(
+                ctx.gameStatDB.get("sp_step_top10")
+            )}`,
             Extra.HTML().markup((m) => m.inlineKeyboard([m.callbackButton("🎮 Play Again", "FIN_PLAY_AGAIN")]))
         );
         deleteSessionFeatures(ctx.session);
