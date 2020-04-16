21b0906d0520d4704b78c99b11d24ca42a141966
Ugur <ugurduzel@gmail.com>
2020-04-16 14:56:43 +0300

    everything works done

--- scenes/multiplayer/mp_ongoingScene.js
+++ scenes/multiplayer/mp_ongoingScene.js
@@ -31,73 +31,6 @@ mp_ongoingScene.action("FIN_PLAY_AGAIN", (ctx) => {
     );
 });
 
-mp_ongoingScene.hears(/^[0-9] digits/, (ctx) => {
-    try {
-        if (!storage.has(ctx.chat.id)) {
-            storage.set(ctx.chat.id, {
-                user1: null,
-                user2: null,
-                turn: null,
-            });
-        }
-        let mpGame = storage.get(ctx.chat.id);
-
-        if (
-            mpGame.user1 &&
-            mpGame.user1.hasOwnProperty("number") &&
-            mpGame.user2 &&
-            mpGame.user2.hasOwnProperty("number")
-        ) {
-            return;
-        }
-
-        const level = eval(ctx.match[0][0]);
-
-        console.log(level + " digit level chosen in play again");
-
-        if (level < minLevel || level > maxLevel) {
-            return ctx.reply(
-                "Choose difficulty level",
-                "<p>Plase select from these options!</p>",
-                Extra.HTML().markup((m) =>
-                    m.inlineKeyboard(levels.map((l) => m.callbackButton(`${l} digits`, `${l} digits`)))
-                )
-            );
-        }
-
-        let user1 = { ...mpGame.user1 };
-        let user2 = { ...mpGame.user2 };
-
-        user1.number = generateRandomNumber(level);
-        user2.number = generateRandomNumber(level);
-
-        console.log(`${user1.name}'s number is ${user1.number}`);
-        console.log(`${user2.name}'s number is ${user2.number}`);
-
-        user1.guesses = 1;
-        user2.guesses = 1;
-
-        user1.history = [];
-        user2.history = [];
-
-        let copy = { ...mpGame };
-        copy.user1 = user1;
-        copy.user2 = user2;
-        copy.turn = user1.id;
-
-        storage.set(ctx.chat.id, copy);
-        return ctx.reply(
-            `Two different ${user1.number} digit numbers have been set for you guys.\n\nStart guessing....`,
-            Extra.HTML().markup((m) =>
-                m.inlineKeyboard([m.callbackButton("Get History", "History"), m.callbackButton("Quit", "Quit")])
-            )
-        );
-    } catch (ex) {
-        console.log("Unexpected error. " + ex);
-        unexpectedErrorKeyboard(ctx);
-    }
-});
-
 mp_ongoingScene.action("Quit", (ctx) => {
     //deleteSessionFeatures(ctx);
     return ctx.reply(
@@ -156,8 +89,80 @@ mp_ongoingScene.enter((ctx) => {
     }
 });
 
+function restartGame(ctx) {
+    try {
+        if (!storage.has(ctx.chat.id)) {
+            storage.set(ctx.chat.id, {
+                user1: null,
+                user2: null,
+                turn: null,
+            });
+        }
+        let mpGame = storage.get(ctx.chat.id);
+
+        if (
+            mpGame.user1 &&
+            mpGame.user1.hasOwnProperty("number") &&
+            mpGame.user2 &&
+            mpGame.user2.hasOwnProperty("number")
+        ) {
+            return;
+        }
+
+        const level = eval(ctx.match[0][0]);
+
+        if (level < minLevel || level > maxLevel) {
+            return ctx.reply(
+                "Choose difficulty level",
+                "<p>Plase select from these inline options!</p>",
+                Extra.HTML().markup((m) =>
+                    m.inlineKeyboard(levels.map((l) => m.callbackButton(`${l} digits`, `${l} digits`)))
+                )
+            );
+        }
+
+        console.log(level + " digit level chosen in play again");
+
+        let user1 = { ...mpGame.user1 };
+        let user2 = { ...mpGame.user2 };
+
+        user1.number = generateRandomNumber(level);
+        user2.number = generateRandomNumber(level);
+
+        console.log(`${user1.name}'s number is ${user1.number}`);
+        console.log(`${user2.name}'s number is ${user2.number}`);
+
+        user1.guesses = 1;
+        user2.guesses = 1;
+
+        user1.history = [];
+        user2.history = [];
+
+        let copy = { ...mpGame };
+        copy.user1 = user1;
+        copy.user2 = user2;
+        copy.turn = user1.id;
+
+        storage.set(ctx.chat.id, copy);
+        return ctx.reply(
+            `Two different ${user1.number} digit numbers have been set for you guys.\n\nStart guessing....`,
+            Extra.HTML().markup((m) =>
+                m.inlineKeyboard([m.callbackButton("Get History", "History"), m.callbackButton("Quit", "Quit")])
+            )
+        );
+    } catch (ex) {
+        console.log("Unexpected error. " + ex);
+        unexpectedErrorKeyboard(ctx);
+    }
+}
+
 mp_ongoingScene.hears(/.*/, (ctx) => {
     try {
+        console.log(ctx.message.text);
+        if (ctx.message.text === "3 digits" || ctx.message.text === "4 digits" || ctx.message.text === "5 digits") {
+            return restartGame(ctx);
+        }
+
         if (storage.get(ctx.chat.id).turn && storage.get(ctx.chat.id).turn !== ctx.from.id) {
             return ctx.reply(
                 "It's not your turn. " + extractUsername(ctx),
