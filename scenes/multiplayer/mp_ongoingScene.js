4931428797bcc7201a9b390bee466d6705ef0384
Ugur <ugurduzel@gmail.com>
2020-04-16 17:17:03 +0300

    fin

--- scenes/multiplayer/mp_ongoingScene.js
+++ scenes/multiplayer/mp_ongoingScene.js
@@ -29,9 +29,35 @@ mp_ongoingScene.action("FIN_PLAY_AGAIN", (ctx) => {
 
 mp_ongoingScene.action(/^[0-9] mp_digits/, (ctx) => {
     try {
+        if (!storage.has(ctx.chat.id)) {
+            storage.set(ctx.chat.id, {
+                user1: null,
+                user2: null,
+                turn: null,
+            });
+        }
         let mpGame = storage.get(ctx.chat.id);
 
+        if (
+            mpGame.user1 &&
+            mpGame.user1.hasOwnProperty("number") &&
+            mpGame.user2 &&
+            mpGame.user2.hasOwnProperty("number")
+        ) {
+            return;
+        }
+
         const level = eval(ctx.match[0][0]);
+
+        if (level < minLevel || level > maxLevel) {
+            return ctx.reply(
+                "Choose difficulty level",
+                "<p>Plase select from these inline options!</p>",
+                Extra.HTML().markup((m) =>
+                    m.inlineKeyboard(levels.map((l) => m.callbackButton(`${l} digits`, `${l} digits`)))
+                )
+            );
+        }
 
         console.log(level + " digit level chosen in play again");
 
